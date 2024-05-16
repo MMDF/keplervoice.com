4172c132b92d064e29e7565bb7bc14906cd48e0f
vishal895 <vishal9389083502@gmail.com>
2024-05-16 11:28:17 +0530

    add job profile

--- src/components/CareerTable/Careertable.js
+++ src/components/CareerTable/Careertable.js
@@ -28,6 +28,27 @@ function Careertable() {
             <Link to="https://forms.gle/hH2tkoBES5BAhTN28" target="_blank" className="font_apply"><p>Apply Now</p></Link>
             </td>
           </tr>
+          <tr>
+            <td>Human Resourse (HR)</td>
+            <td>Full Time</td>
+            <td>
+            <Link to="https://forms.gle/hH2tkoBES5BAhTN28" target="_blank" className="font_apply"><p>Apply Now</p></Link>
+            </td>
+          </tr>
+          <tr>
+            <td>Data Analyst</td>
+            <td>Full Time</td>
+            <td>
+            <Link to="https://forms.gle/hH2tkoBES5BAhTN28" target="_blank" className="font_apply"><p>Apply Now</p></Link>
+            </td>
+          </tr>
+          <tr>
+            <td>Customer Care Executive</td>
+            <td>Full Time</td>
+            <td>
+            <Link to="https://forms.gle/hH2tkoBES5BAhTN28" target="_blank" className="font_apply"><p>Apply Now</p></Link>
+            </td>
+          </tr>
         </tbody>
 
       </table>
